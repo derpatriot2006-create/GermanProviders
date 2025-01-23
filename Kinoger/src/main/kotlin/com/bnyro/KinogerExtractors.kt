76580e41b64136e8bcc292e06d38f07a1688f59b
Bnyro <bnyro@tutanota.com>
2025-01-23 10:16:43 +0100

    kinoger, serienstream, moflix: init

---
+++ Kinoger/src/main/kotlin/com/bnyro/KinogerExtractors.kt
@@ -0,0 +1,8 @@
+package com.bnyro
+
+import com.lagradost.cloudstream3.extractors.Chillx
+
+class Kinogeru : Chillx() {
+    override val name = "Kinoger"
+    override val mainUrl = "https://kinoger.ru"
+}
---
+++ Serienstream/src/main/kotlin/com/bnyro/SerienstreamPlugin.kt
@@ -0,0 +1,16 @@
+
+package com.bnyro
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.Urochsunloath
+
+@CloudstreamPlugin
+class SerienstreamPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(Serienstream())
+        registerExtractorAPI(Urochsunloath())
+    }
+}
