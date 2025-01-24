fe95199570e3213cb8db40fe22214cb515704589
Bnyro <bnyro@tutanota.com>
2025-01-24 11:02:18 +0100

    kinoger: use chillx extractor from phisher repo

--- Kinoger/src/main/kotlin/com/bnyro/KinogerExtractors.kt
+++ Kinoger/src/main/kotlin/com/bnyro/KinogerExtractors.kt
@@ -1,8 +1,108 @@
 package com.bnyro
 
-import com.lagradost.cloudstream3.extractors.Chillx
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.USER_AGENT
+import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.extractors.Supervideo
 import com.lagradost.cloudstream3.extractors.Vidguardto
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.utils.Qualities
+import java.util.zip.Inflater
+
+open class Chillx : ExtractorApi() {
+    override val name = "Chillx"
+    override val mainUrl = "https://chillx.top"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val res = app.get(url).toString()
+        val encodedString =
+            Regex("Encrypted\\s*=\\s*'(.*?)';").find(res)?.groupValues?.get(1).orEmpty()
+        val decoded = decodeEncryptedData(encodedString)
+
+        val m3u8 = Regex("file:\\s*\"(.*?)\"").find(decoded.orEmpty())?.groupValues?.get(1).orEmpty()
+        val header = mapOf(
+            "accept" to "*/*",
+            "accept-language" to "en-US,en;q=0.5",
+            "Origin" to mainUrl,
+            "Accept-Encoding" to "gzip, deflate, br",
+            "Connection" to "keep-alive",
+            "Sec-Fetch-Dest" to "empty",
+            "Sec-Fetch-Mode" to "cors",
+            "Sec-Fetch-Site" to "cross-site",
+            "user-agent" to USER_AGENT,
+        )
+        callback.invoke(
+            ExtractorLink(
+                name,
+                name,
+                m3u8,
+                mainUrl,
+                Qualities.P1080.value,
+                INFER_TYPE,
+                headers = header
+            )
+        )
+
+        val subtitles = extractSrtSubtitles(decoded.orEmpty())
+        subtitles.forEachIndexed { _, (language, url) ->
+            subtitleCallback.invoke(SubtitleFile(language, url))
+        }
+    }
+
+    private fun extractSrtSubtitles(subtitle: String): List<Pair<String, String>> {
+        val regex = """\[([^]]+)](https?://[^\s,]+\.srt)""".toRegex()
+
+        return regex.findAll(subtitle).map { match ->
+            val (language, url) = match.destructured
+            language.trim() to url.trim()
+        }.toList()
+    }
+
+
+    private fun decodeEncryptedData(encryptedString: String?): String? {
+        if (encryptedString == null) return null
+
+        return try {
+            val decodedBytes =
+                android.util.Base64.decode(encryptedString, android.util.Base64.DEFAULT)
+
+            val decodedCharacters = decodedBytes.map { byte ->
+                val binaryRepresentation = byte.toUByte().toString(2).padStart(8, '0')
+                val reversedBinary = binaryRepresentation.reversed()
+                reversedBinary.toInt(2).toByte()
+            }
+            val byteArray = ByteArray(decodedCharacters.size) { decodedCharacters[it] }
+            val decompressedData = Inflater().run {
+                setInput(byteArray)
+                val output = ByteArray(1024 * 4)
+                val decompressedSize = inflate(output)
+                output.copyOf(decompressedSize).toString(Charsets.UTF_8)
+            }
+            val specialToAlphabetMap = mapOf(
+                '!' to 'a', '@' to 'b', '#' to 'c', '$' to 'd', '%' to 'e',
+                '^' to 'f', '&' to 'g', '*' to 'h', '(' to 'i', ')' to 'j'
+            )
+            val processedData = decompressedData.map { char ->
+                specialToAlphabetMap[char] ?: char
+            }.joinToString("")
+            val finalDecodedData =
+                android.util.Base64.decode(processedData, android.util.Base64.DEFAULT)
+                    .toString(Charsets.UTF_8)
+            finalDecodedData
+        } catch (e: Exception) {
+            println("Error decoding string: ${e.message}")
+            null
+        }
+    }
+}
 
 class KinogerRu : Chillx() {
     override val name = "Kinoger"
