d3b4d877fad242705d8fd8239115ea33e5c5845a
Bnyro <bnyro@tutanota.com>
2025-01-28 12:24:15 +0100

    ard: support for series

--- ARD/src/main/kotlin/com/bnyro/ARD.kt
+++ ARD/src/main/kotlin/com/bnyro/ARD.kt
@@ -3,20 +3,25 @@ package com.bnyro
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.addDate
+import com.lagradost.cloudstream3.apmap
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.mainPageOf
+import com.lagradost.cloudstream3.newEpisode
 import com.lagradost.cloudstream3.newHomePageResponse
 import com.lagradost.cloudstream3.newMovieLoadResponse
 import com.lagradost.cloudstream3.newMovieSearchResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.INFER_TYPE
 import com.lagradost.cloudstream3.utils.Qualities
 
 
@@ -48,7 +53,7 @@ open class ARD : MainAPI() {
         request: MainPageRequest
     ): HomePageResponse {
         val response =
-            app.get("${mainUrl}/page-gateway/widgets/ard/editorials/${request.data}?pageSize=${PAGE_SIZE}")
+            app.get("${mainUrl}/page-gateway/widgets/ard/editorials/${request.data}?pageSize=${PAGE_SIZE}&pageNumber=${page - 1}")
                 .parsed<Editorial>()
 
         return newHomePageResponse(
@@ -69,10 +74,7 @@ open class ARD : MainAPI() {
 
         return newMovieSearchResponse(
             name = this.mediumTitle ?: this.shortTitle ?: return null,
-            url = ItemInfo(
-                this.links?.target?.id ?: this.id,
-                isLive = type == TvType.Live
-            ).toJson(),
+            url = ItemInfo(this.links?.target?.id ?: this.id, type).toJson(),
             type = type,
         ) {
             this.posterUrl =
@@ -84,23 +86,64 @@ open class ARD : MainAPI() {
     override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val response =
-            app.get("$mainUrl/search-system/search/vods/ard?query=${query}&pageSize=${PAGE_SIZE}&platform=MEDIA_THEK&sortingCriteria=SCORE_DESC")
+        return listOf("shows", "vods").apmap { type ->
+            app.get("$mainUrl/search-system/search/${type}/ard?query=${query}&pageSize=${PAGE_SIZE}&platform=MEDIA_THEK&sortingCriteria=SCORE_DESC")
                 .parsed<Search>()
-
-        return response.teasers.mapNotNull { it.toSearchResponse() }
+                .teasers
+                .mapNotNull { it.toSearchResponse() }
+        }.flatten()
     }
 
-    override suspend fun load(url: String): LoadResponse? {
-        val (episodeId, isLive) = parseJson<ItemInfo>(url)
+    private suspend fun loadSeries(url: String, seriesId: String): LoadResponse {
+        val response = app.get(
+            "${mainUrl}/page-gateway/pages/ard/grouping/${seriesId}?seasoned=true&embedded=true"
+        )
+            .parsed<GroupingResponse>()
 
-        val response =
-            app.get(
-                "${mainUrl}/page-gateway/pages/ard/item/${episodeId}?embedded=false&mcV6=true"
-            )
-                .parsed<DetailsResponse>()
+        val type = getType(response.coreAssetType)
+        val episodes =
+            response.widgets.filter { it.compilationType == "itemsOfSeason" }.map { season ->
+                season.teasers.mapIndexed { index, episode ->
+                    newEpisode(ItemInfo(episode.links?.target?.id ?: episode.id, type)) {
+                        this.name = episode.mediumTitle ?: episode.longTitle
+                        this.season = season.seasonNumber?.toIntOrNull()
+                        this.runTime = episode.duration?.div(60)?.toInt()
+                        this.episode = index + 1
+                        this.posterUrl = episode.images.values.firstOrNull()?.src
+                        addDate(episode.broadcastedOn?.take(10))
+                    }
+                }
+            }.flatten()
 
-        val streamInfo = response.widgets.firstOrNull { it.type.startsWith("player") }
+        return newTvSeriesLoadResponse(
+            name = response.title,
+            url = url,
+            type = type,
+            episodes = episodes
+        ) {
+            this.posterUrl = (response.heroImage ?: response.image)?.src
+            this.plot = response.synopsis
+            response.trailer?.links?.target?.id?.let {
+                addTrailer(ItemInfo(it, TvType.TvSeries).toJson())
+            }
+        }
+    }
+
+    private suspend fun fetchEpisodeInfo(episodeId: String): DetailsResponse {
+        return app.get(
+            "${mainUrl}/page-gateway/pages/ard/item/${episodeId}?embedded=false&mcV6=true"
+        )
+            .parsed<DetailsResponse>()
+    }
+
+    private fun findPlayerInfo(widgets: List<Widget>): Widget? {
+        return widgets.firstOrNull { it.type.startsWith("player") }
+    }
+
+    private suspend fun loadEpisode(url: String, episodeId: String): LoadResponse {
+        val response = fetchEpisodeInfo(episodeId)
+
+        val streamInfo = findPlayerInfo(response.widgets)
         val embedded = streamInfo?.mediaCollection?.embedded
         return newMovieLoadResponse(
             name = response.title,
@@ -116,13 +159,29 @@ open class ARD : MainAPI() {
         }
     }
 
+    override suspend fun load(url: String): LoadResponse? {
+        val (id, type) = parseJson<ItemInfo>(url)
+
+        return when (type) {
+            TvType.TvSeries -> loadSeries(url, id)
+            else -> loadEpisode(url, id)
+        }
+    }
+
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val embedded = parseJson<Embedded>(data)
+        var embedded = tryParseJson<Embedded>(data)
+        if (embedded == null) {
+            val (id, _) = parseJson<ItemInfo>(data)
+
+            embedded = findPlayerInfo(fetchEpisodeInfo(id).widgets)?.mediaCollection?.embedded
+        }
+
+        if (embedded == null) throw IllegalArgumentException("Couldn't find episode info!")
         if (embedded.streams.isEmpty()) return false
 
         for (stream in embedded.streams) {
@@ -156,7 +215,7 @@ open class ARD : MainAPI() {
 
     data class ItemInfo(
         val id: String,
-        val isLive: Boolean
+        val type: TvType
     )
 
     data class Search(
@@ -223,13 +282,10 @@ open class ARD : MainAPI() {
         val synopsis: String?,
         val title: String,
         val type: String,
-        @JsonProperty("aZContent")
-        val aZcontent: Boolean?,
         val compilationType: String?,
         val size: String?,
-        val swipeable: Boolean?,
-        val titleVisible: Boolean?,
-        val userVisibility: String?,
+        val seasonNumber: String?,
+        val teasers: List<Teaser> = emptyList()
     )
 
     data class Image(
@@ -323,6 +379,24 @@ open class ARD : MainAPI() {
         val size: String?,
         val teasers: List<Teaser> = emptyList(),
         val type: String?,
+    )
+
+    data class GroupingResponse(
+        val id: String?,
+        val title: String,
+        val synopsis: String?,
+        val heroImage: Image?,
+        val image: Image?,
+        val widgets: List<Widget> = emptyList(),
+        val coreAssetType: String?,
+        val trailer: Trailer?,
+    )
+
+    data class Trailer(
+        val duration: Long?,
+        val links: Links?,
+        val title: String?,
+        val synopsis: String?,
     )
 
     companion object {
