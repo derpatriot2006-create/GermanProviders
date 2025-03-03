19aa3ef02d61a2ea5a4b2d1daba4a4ee21934977
Bnyro <bnyro@tutanota.com>
2025-03-03 13:17:49 +0100

    einschalten: add filemoon extractor

--- EinschaltenIn/src/main/kotlin/com/bnyro/EinschaltenInPlugin.kt
+++ EinschaltenIn/src/main/kotlin/com/bnyro/EinschaltenInPlugin.kt
@@ -4,6 +4,7 @@ package com.bnyro
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import com.lagradost.cloudstream3.extractors.FileMoonSx
 
 @CloudstreamPlugin
 class EinschaltenInPlugin: Plugin() {
@@ -11,5 +12,6 @@ class EinschaltenInPlugin: Plugin() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(EinschaltenInProvider())
         registerExtractorAPI(D000dCom())
+        registerExtractorAPI(FileMoonSx())
     }
 }
